f25a8a2d3e16e72a097ea49853528d17a2c8c2f4
Huang <jjj08254@gmail.com>
2020-05-12 01:52:28 -0400

    react_to_do_list

---
+++ react_to_do_list/precache-manifest.2088d39b270467d2a9d7cef5f8dcaac0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7142ea65db55b7882ebcb6a500dc4dec",
+    "url": "/react_to_do_list/index.html"
+  },
+  {
+    "revision": "aafb839c3526a4c76254",
+    "url": "/react_to_do_list/static/js/2.34a9d68b.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/react_to_do_list/static/js/2.34a9d68b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3ad2d5750bc5a83411b",
+    "url": "/react_to_do_list/static/js/main.5582b6fa.chunk.js"
+  },
+  {
+    "revision": "64d31b34c41ae392d0c0",
+    "url": "/react_to_do_list/static/js/runtime-main.7f8de59c.js"
+  }
+]);
