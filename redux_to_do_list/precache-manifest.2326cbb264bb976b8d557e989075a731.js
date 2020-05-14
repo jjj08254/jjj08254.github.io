a62d52c0b675ead2e5e6634f9177c5c16470b779
Huang <jjj08254@gmail.com>
2020-05-14 02:00:59 -0400

    redux_to_do_list

---
+++ redux_to_do_list/precache-manifest.2326cbb264bb976b8d557e989075a731.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be857806660982773b673531f2224284",
+    "url": "/redux_to_do_list/index.html"
+  },
+  {
+    "revision": "008f39281768ddb72255",
+    "url": "/redux_to_do_list/static/js/2.51690031.chunk.js"
+  },
+  {
+    "revision": "996e97a9c59b2ad74d9e36e982ab30fe",
+    "url": "/redux_to_do_list/static/js/2.51690031.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6a53db7aa8bb2a1e37f",
+    "url": "/redux_to_do_list/static/js/main.04ef6c46.chunk.js"
+  },
+  {
+    "revision": "5e16a00d539944f9e641",
+    "url": "/redux_to_do_list/static/js/runtime-main.9141d556.js"
+  }
+]);
